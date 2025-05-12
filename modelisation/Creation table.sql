e5ca6b3aaf4e66c33cccf17c0220b12fc449e048
Flobio85 <grelierflorian4@gmail.com>
2025-05-12 14:00:44 +0000

    modelisation

--- modelisation/Creation table.sql
+++ modelisation/Creation table.sql
@@ -1,27 +1,28 @@
 CREATE TABLE Utilisateur(
-   IdUtilisateur COUNTER,
-   Nom VARCHAR(50),
-   Prenom VARCHAR(50),
-   Email VARCHAR(100),
+   IdUtilisateur SERIAL,
+   Nom VARCHAR(50) ,
+   Prenom VARCHAR(50) ,
+   Email VARCHAR(100) ,
    PRIMARY KEY(IdUtilisateur)
 );
 
 CREATE TABLE Materiel(
-   IdMateriel COUNTER,
-   Nom VARCHAR(100),
-   Categorie VARCHAR(50),
-   QuantiteDisponible INT,
+   IdMateriel SERIAL,
+   Nom VARCHAR(100) ,
+   Categorie VARCHAR(50) ,
+   QuantiteDisponible INTEGER,
    PRIMARY KEY(IdMateriel)
 );
 
 CREATE TABLE Reservation(
-   IdReservation COUNTER,
-   DateDebut DATETIME,
-   DateFin DATETIME,
-   Quantite INT,
-   IdMateriel INT,
-   IdUtilisateur INT,
+   IdReservation SERIAL,
+   DateDebut TIMESTAMP,
+   DateFin TIMESTAMP,
+   Quantite INTEGER,
+   IdMateriel INTEGER,
+   IdUtilisateur INTEGER,
    PRIMARY KEY(IdReservation),
    FOREIGN KEY(IdMateriel) REFERENCES Materiel(IdMateriel),
    FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateur(IdUtilisateur)
 );
+
